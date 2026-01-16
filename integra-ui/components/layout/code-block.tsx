dc7c4d4d0abd7befd62602880211c81a262bac31
taekilKim <taekil.design@gmail.com>
2026-01-16 14:09:05 +0900

    add use client in code-block.tsx

--- integra-ui/components/layout/code-block.tsx
+++ integra-ui/components/layout/code-block.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import * as React from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism"
